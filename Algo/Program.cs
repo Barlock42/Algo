78c8e4d08056f01944102169c1705b903e80057c
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-03-27 21:00:30 +0900

    solved

--- Algo/Program.cs
+++ Algo/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+//If target is not found in the array, return [-1, -1].
+
+//You must write an algorithm with O(log n) runtime complexity.
+
+
+using System;
+using System.Linq;
 
 namespace Algo
 {
@@ -16,15 +24,42 @@ namespace Algo
 
         static public int[] SearchRange(int[] nums, int target)
         {
-            int left = 0;
-            int right = nums.Length-1;
+            int leftIndex = FindIndex(nums, target, true); // find the leftmost index of the target
+            int rightIndex = FindIndex(nums, target, false); // find the rightmost index of the target
+            return new int[] { leftIndex, rightIndex };
+        }
 
-            int middle = (left + right) / 2;
-
-
-
-
-            return new int[] { -1, -1 };
+        // helper method to find the leftmost or rightmost index of the target using binary search
+        static private int FindIndex(int[] nums, int target, bool isLeft)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            int index = -1;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == target)
+                {
+                    index = mid;
+                    if (isLeft)
+                    { // look for the leftmost index of the target
+                        end = mid - 1;
+                    }
+                    else
+                    { // look for the rightmost index of the target
+                        start = mid + 1;
+                    }
+                }
+                else if (nums[mid] < target)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return index;
         }
     }
 }
