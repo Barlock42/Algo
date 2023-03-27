1b7609149ee46c00f215be5cab02ea76a290170b
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-03-27 20:34:24 +0900

    Algo
    Algo

---
+++ Algo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algo
+{
+    public class Algo
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 5, 7, 7, 8, 8, 10 };
+            SearchRange(nums, 8);
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+        static public int[] SearchRange(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length-1;
+
+            int middle = (left + right) / 2;
+
+
+
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+
+namespace Algo.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] nums = { 5, 7, 7, 8, 8, 10 };
+            int target = 8;
+            int[] expected = { 3, 4 };
+            int[] result = Algo.SearchRange(nums, target);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] nums = { 5, 7, 7, 8, 8, 10 };
+            int target = 6;
+            int[] expected = { -1, -1 };
+            int[] result = Algo.SearchRange(nums, target);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] nums = { };
+            int target = 0;
+            int[] expected = { -1, -1 };
+            int[] result = Algo.SearchRange(nums, target);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
