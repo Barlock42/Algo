b87593b653baa7402684d6ef18b5a960dbf99e6e
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2023-06-27 20:36:27 +0900

    format

--- Algo/linked list/NthNode.cs
+++ Algo/linked list/NthNode.cs
@@ -9,22 +9,26 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
         var dummy = new ListNode(0, head);
         var left = dummy;
         var right = head;
-        
-        while(n > 0) {
+
+        while (n > 0)
+        {
             right = right.next;
             n--;
         }
-        
-        while(right != null) {
+
+        while (right != null)
+        {
             left = left.next;
             right = right.next;
         }
-        
+
         // delete
         left.next = left.next.next;
         return dummy.next;
